5fea235eaa789f1afe16f5414ed3037b0135d1fa
Tommaso Paoli <paoli7612@gmail.com>
2018-06-01 21:24:15 +0200

    improved last

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from player import Player
+from window import Window
 
 def main():
+    w = Window()
     p1 = Player("Mario", 5, 7)
     p2 = Player("Giovanni", 9, 7)
 
+    w.draw()
+    p1.draw(w.screen)
+    w.flip()
     print(p1)
     print(p2)
+
+    import time
+    time.sleep(2)
 
 
 if __name__ == "__main__":
