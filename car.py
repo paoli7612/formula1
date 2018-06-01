8fb5ac0a024a135bcb6b04f106179e578267b211
Tommaso Paoli <paoli7612@gmail.com>
2018-06-01 20:28:25 +0200

    starter classes

---
+++ car.py
@@ -0,0 +1,6 @@
+from pos import Pos
+
+class Car:
+    def __init__(self, x, y):
+        self.pos = Pos(x,y)
+        self.last_pos = Pos(x,y)
---
+++ player.py
@@ -0,0 +1,6 @@
+from car import Car
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.car = Car(10,10)
