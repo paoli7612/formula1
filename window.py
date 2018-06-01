db5ec93e739e4dce9491a8589d8bea984aece7cc
Tommaso Paoli <paoli7612@gmail.com>
2018-06-01 21:10:10 +0200

    draw grid and test

---
+++ window.py
@@ -0,0 +1,30 @@
+import pygame
+
+import conf
+
+class Window:
+    def __init__(self):
+        self.screen = pygame.display.set_mode(conf.SIZE)
+        pygame.display.set_caption(conf.TITLE)
+
+        self.grid = pygame.Surface(conf.SIZE)
+        self.grid.fill(conf.COLOR_KEY)
+        for x in range(0,conf.WIDTH+1,conf.TILE):
+            pygame.draw.line(self.grid,conf.COLOR_GRID,(x,0),(x,conf.HEIGHT),3)
+        for y in range(0,conf.HEIGHT+1,conf.TILE):
+            pygame.draw.line(self.grid,conf.COLOR_GRID,(0,y),(conf.WIDTH,y),3)
+        self.grid.set_colorkey(conf.COLOR_KEY)
+
+    def draw(self):
+        self.screen.fill(conf.COLOR_BGROUND)
+        self.screen.blit(self.grid, (0,0))
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    # test
+    w = Window()
+    w.draw()
+
+    import time
+    time.sleep(2)
