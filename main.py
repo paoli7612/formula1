2856c2078eeb83ea51154ad1d7fc1999e177daca
Tommaso Paoli <paoli7612@gmail.com>
2018-06-02 21:10:49 +0200

    resize window

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from match import Match
 def main():
     p1 = Player("nome1", 10, 10, "red")
     p2 = Player("nome2", 10, 12, "blue")
-    m = Match(p1)
+    m = Match(p1,p2)
     m.start()
 
 if __name__ == "__main__":
